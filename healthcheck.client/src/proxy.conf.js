19fa17f624ca01eefc42ed5504eece135566008f
ahmet bilgic <ahmet_bilgic@yahoo.com>
2024-04-03 03:10:43 +0300

    working project

--- healthcheck.client/src/proxy.conf.js
+++ healthcheck.client/src/proxy.conf.js
@@ -1,14 +1,14 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7159';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:40443';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
     ],
-    target,
+    target: "https://localhost:40443",
     secure: false
   }
 ]
